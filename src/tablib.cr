97b1880bda409e32df62297e07b590539bb9e555
weaming <garden.yuen@gmail.com>
2018-11-16 14:31:07 +0800

    move api to tabli.cr

--- src/tablib.cr
+++ src/tablib.cr
@@ -1,9 +1,73 @@
 require "./parser"
 require "./cli"
 
-# TODO: Write documentation for `Tablib`
+# Parse betwwen YAML, JSON, CSV
 module Tablib
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  def self.yaml_json(path : String)
+    text = read_file path
+    if text.size == 0
+      exit 3
+    end
+
+    if text.strip.starts_with? /[[{]/ # is json
+      begin
+        data = JSON.parse text
+      rescue e
+        puts e
+        exit 4
+      end
+
+      data = JSONFormatter.parse(data)
+      puts data.to_yaml
+    else # is yaml
+      begin
+        data = YAML.parse text
+      rescue e
+        puts e
+        exit 5
+      end
+
+      data = JSONFormatter.parse(data)
+      puts data.to_pretty_json
+    end
+  end
+
+  def self.csv_json(path : String)
+    text = read_file path
+    if text.size == 0
+      exit 3
+    end
+
+    if text.starts_with? /[[{]/ # is json
+      begin
+        data = JSON.parse text
+      rescue e
+        puts e
+        exit 4
+      end
+
+      # output csv
+      data = JSONFormatter.parse data
+      case data
+      when Array(JSONFormatter::TabAny)
+        str = JSONFormatter.to_csv data
+        puts str
+      else
+        puts "invalid csv 2d structure"
+        exit 6
+      end
+    else # is csv
+      begin
+        data = CSV.parse text
+      rescue e
+        puts e
+        exit 5
+      end
+
+      data = JSONFormatter.parse_csv data
+      puts data.to_pretty_json
+    end
+  end
 end
